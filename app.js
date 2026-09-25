2ed5fcb9c86bdbfba03737b18c85bd9c6c81dbb5
Shraya N L <shraya932@gmail.com>
2026-09-25 22:15:49 +0530

    Add custom logging middleware and endpoints for products and categories

--- app.js
+++ app.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
-// Dynamic GET route using route parameters and query parameters
-app.get('/welcome/:username', (req, res) => {
-    const { username } = req.params;
-    const { role } = req.query;
+// Custom Logging Middleware
+app.use((req, res, next) => {
+    console.log(`${req.method} request made to ${req.url}`);
+    next();
+});
 
-    if (role) {
-        res.send(`Welcome ${username}, your role is ${role}`);
-    } else {
-        res.send(`Welcome ${username}`);
-    }
+// 1. GET /products
+app.get('/products', (req, res) => {
+    res.send("Here is the list of all products.");
+});
+
+// 2. POST /products
+app.post('/products', (req, res) => {
+    res.send("A new product has been added.");
+});
+
+// 3. GET /categories
+app.get('/categories', (req, res) => {
+    res.send("Here is the list of all categories.");
+});
+
+// 4. POST /categories
+app.post('/categories', (req, res) => {
+    res.send("A new category has been created.");
 });
 
 // Start server

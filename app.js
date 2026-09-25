84062a627a209922f90b591acc514a398d283dca
Shraya N L <shraya932@gmail.com>
2026-09-25 22:08:27 +0530

    Add routes for orders and users

--- app.js
+++ app.js
@@ -2,17 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Custom Middleware Function
-const addUserMiddleware = (req, res, next) => {
-    req.user = "Guest";
-    next();
-};
-
-// Route using the middleware
-app.get('/welcome', addUserMiddleware, (req, res) => {
-    res.send(`<h1>Welcome, ${req.user}!</h1>`);
+// 1. GET /orders
+app.get('/orders', (req, res) => {
+    res.send("Here is the list of all orders.");
 });
 
+// 2. POST /orders
+app.post('/orders', (req, res) => {
+    res.send("A new order has been created.");
+});
+
+// 3. GET /users
+app.get('/users', (req, res) => {
+    res.send("Here is the list of all users.");
+});
+
+// 4. POST /users
+app.post('/users', (req, res) => {
+    res.send("A new user has been added.");
+});
+
+// Start server
 app.listen(PORT, () => {
-    console.log(`Server is up and running on port ${PORT}! Ready to handle requests.`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });

0f6592d1452690c54d69c78af723381dc76e5d73
Shraya N L <shraya932@gmail.com>
2026-09-25 21:13:32 +0530

    Complete Node.js request response task

--- server.js
+++ server.js
@@ -1,32 +1,92 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
 
-    if (req.url === '/home') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
+    // Show the form and all messages
+    if (req.url === '/' && req.method === 'GET') {
+
+        fs.readFile('message', 'utf8', (err, data) => {
+
+            if (err) {
+                data = '';
+            }
+
+            const messages = data
+                .split('\n')
+                .filter(msg => msg.trim() !== '')
+                .reverse();
+
+            let messageList = '';
+
+            messages.forEach(msg => {
+                messageList += `<p>${msg}</p>`;
+            });
+
+            res.writeHead(200, {
+                'Content-Type': 'text/html'
+            });
+
+            res.end(`
+                <html>
+                <body>
+                    <h1>Messages</h1>
+
+                    ${messageList}
+
+                    <form method="POST" action="/message">
+                        <input 
+                            type="text" 
+                            name="message"
+                            placeholder="Enter message"
+                            required
+                        >
+                        <button type="submit">Send</button>
+                    </form>
+                </body>
+                </html>
+            `);
         });
-        res.end('<h1>Welcome home</h1>');
     }
 
-    else if (req.url === '/about') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
-        });
-        res.end('<h1>Welcome to About Us</h1>');
-    }
+    // Receive and save a new message
+    else if (req.url === '/message' && req.method === 'POST') {
 
-    else if (req.url === '/node') {
-        res.writeHead(200, {
-            'Content-Type': 'text/html'
+        let body = '';
+
+        req.on('data', chunk => {
+            body += chunk;
         });
-        res.end('<h1>Welcome to my Node Js project</h1>');
+
+        req.on('end', () => {
+
+            const message = decodeURIComponent(
+                body.split('=')[1].replace(/\+/g, ' ')
+            );
+
+            fs.appendFile('message', message + '\n', err => {
+
+                if (err) {
+                    res.writeHead(500);
+                    res.end('Error saving message');
+                    return;
+                }
+
+                // Redirect with 302
+                res.writeHead(302, {
+                    'Location': '/'
+                });
+
+                res.end();
+            });
+        });
     }
 
     else {
         res.writeHead(404, {
             'Content-Type': 'text/html'
         });
+
         res.end('<h1>Page Not Found</h1>');
     }
 
